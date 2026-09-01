898c86d7fc74c87fcdbf3da05e72b80733d29984
unknown <tanishque30@gmail.com>
2026-09-01 22:45:39 +0530

    Classes and Object

---
+++ internalTest-01/1stcode.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student{
+    private:
+    int roll,phy,chem,math;
+    string name;
+    public:
+Student(string n,int r,int p,int c,int m){
+    name=n;
+    roll=r;
+    phy=p;
+    chem=c;
+    math=m;
+}
+void display(){
+    cout<<"Student name"<<name<<endl;
+    cout<<"Student roll no "<<roll<<endl;
+    cout<<"Mark in Physic"<<phy<<endl;
+    cout<<"Mark in Chemistry"<<chem<<endl;
+    cout<<"Mark in Maths"<<math;
+}
+};
+int main(){
+    Student st("Tanishque",208,89,96,92);
+    st.display();
+}
