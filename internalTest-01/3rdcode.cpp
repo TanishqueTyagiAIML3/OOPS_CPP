a8a067020b3c4d3505a57b1b0cb42dd472a3dcd5
unknown <tanishque30@gmail.com>
2026-09-01 23:59:40 +0530

    Updated

--- internalTest-01/3rdcode.cpp
+++ internalTest-01/3rdcode.cpp
@@ -1,28 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-void callbyvalue(int a,int b)
+void callbyvalue(int a, int b)
 {
-    cout<<"Value before Swap"<<a<<" "<<b<<endl;
-    int x=0;
-    x=a;
-    a=b;
-    b=x;
-    cout<<"Value after Swap"<<a<<" "<<b<<endl;
+    cout << "Value before Swap" << a << " " << b << endl;
+    int x = 0;
+    x = a;
+    a = b;
+    b = x;
+    cout << "Value after Swap" << a << " " << b << endl;
 }
-void callbyreference(int *a,int *b){
-    int x=0;
-    x=*a;
-    *a=*b;
-    *b=x;
+void callbyreference(int *a, int *b)
+{
+    int x = 0;
+    x = *a;
+    *a = *b;
+    *b = x;
 }
-int main(){
-    int a,b;
-    cin>>a>>b;
-    cout<<"Call By Value :"<<endl;
-    callbyvalue(a,b);
-    cout<<"Call By Reference:"<<endl;
-    cout<<"Value before swap "<<a<<" "<<b<<endl;
-    callbyreference(&a,&b);
-    cout<<"Value after swap "<<a<<" "<<b;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << "Call By Value :" << endl;
+    callbyvalue(a, b);
+    cout << "Call By Reference:" << endl;
+    cout << "Value before swap " << a << " " << b << endl;
+    callbyreference(&a, &b);
+    cout << "Value after swap " << a << " " << b;
     return 0;
 }
