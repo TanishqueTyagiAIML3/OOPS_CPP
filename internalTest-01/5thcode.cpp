09bb28ead7040166c2d951763ff79ad2ecfb8786
unknown <tanishque30@gmail.com>
2026-09-01 23:29:54 +0530

    Object as Argument and Return from func

---
+++ internalTest-01/5thcode.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class product{
+    public:
+    string name;
+    int price;
+    product(string n,int prc){
+        name=n;
+        price=prc;
+    }
+};
+product higherProfit(product p1,product p2){
+    if(p1.price>p2.price)
+    return p1;
+    else
+    return p2;
+}
+int main(){
+    product p1("Mango",130);
+    product p2("Apple",100);
+    auto final=higherProfit(p1,p2);
+    cout<<"Name "<<final.name<<endl;
+    cout<<"Price "<<final.price;
+}
