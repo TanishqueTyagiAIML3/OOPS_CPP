bd3f2a45e1944ad52fc0755250b2fef1736fedb5
unknown <tanishque30@gmail.com>
2026-09-01 23:41:49 +0530

    Friend Func with auto And Range Based Loop

---
+++ internalTest-01/6thcode.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Marks{
+    private:
+    int arr[5];
+    public:
+    void input(){
+        for(int i=0;i<5;i++){
+            cin>>arr[i];
+        }
+    }
+    void display(){
+        cout<<"Marks :";
+        for(auto i:arr)
+        cout<<i<<" ";
+    }
+    friend void totalMarks(Marks st);
+};
+void totalMarks(Marks st);
+int main(){
+    Marks st;
+    st.input();
+    st.display();
+    totalMarks(st);
+}
+void totalMarks(Marks st){
+    int total=0;
+    for(auto i:st.arr){
+        total+=i;
+    }
+    cout<<"total Marks"<<total;
+}
